39901e6581309bbb79053d0fcc35a9a12b1a3c2b
Muhammad Mahad <mahad.dev3@gmail.com>
2025-06-05 20:43:41 +0500

    Updated app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,7 +4,13 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+// if injection token is string then we use @inject in constructor
+  // ****************************************
+// constructor(@Inject('Mail') private email: string[]) {
+  // console.log(this.email);
+// }
+// with the help of private we can access it inside the class anywhere using this.email
+// ****************************************
   @Get()
   getHello(): string {
     return this.appService.getHello();
